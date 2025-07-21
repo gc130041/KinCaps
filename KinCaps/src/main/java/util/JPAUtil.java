b4157898f069ae04c134fa4fc9d1648e823ddb83
ksolo-2022439 <ksolorzano-2022439@kinal.edu.gt>
2025-07-21 17:25:40 -0600

    feat: Añadir clase JpaUtil con patrón Singleton
    Clase que centraliza la configuración y manejo del EntityManagerFactory para evitar múltiples instancias en el sistema.

---
+++ KinCaps/src/main/java/util/JPAUtil.java
@@ -0,0 +1,28 @@
+package util;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAUtil {
+    
+    private static final String PERSISTENCE_UNIT_NAME = "KinCaps_Persistence";
+    private static EntityManagerFactory factory;
+
+    public static EntityManagerFactory getEntityManagerFactory() {
+        if (factory == null) {
+            factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+        }
+        return factory;
+    }
+    
+    public static EntityManager getEntityManager() {
+        return getEntityManagerFactory().createEntityManager();
+    }
+
+    public static void shutdown() {
+        if (factory != null) {
+            factory.close();
+        }
+    }
+}
