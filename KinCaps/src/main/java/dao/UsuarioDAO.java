77a33f1c30686096c35dace927df309f68037f69
ksolo-2022439 <ksolorzano-2022439@kinal.edu.gt>
2025-07-21 17:25:49 -0600

    feat: Añadir método verificarCredenciales en UsuarioDAO
    Permite validar credenciales de usuario y distinguir entre Cliente y Empleado para el proceso de autenticación.

--- KinCaps/src/main/java/dao/UsuarioDAO.java
+++ KinCaps/src/main/java/dao/UsuarioDAO.java
@@ -1,15 +1,43 @@
 package dao;
 
-import java.util.List;
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.NoResultException;
-import javax.persistence.Persistence;
-import javax.persistence.TypedQuery;
+import javax.persistence.ParameterMode;
+import javax.persistence.StoredProcedureQuery;
+import modelo.Cliente;
+import modelo.Empleado;
 import modelo.Usuario;
+import util.JPAUtil;
 
 public class UsuarioDAO {
 
-    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("KinCaps_Persistence");
+    public Usuario verificarCredenciales(String email, String password) {
+        EntityManager em = JPAUtil.getEntityManager();
+        try {
+            StoredProcedureQuery query = em.createStoredProcedureQuery("sp_Login")
+                .registerStoredProcedureParameter("p_email", String.class, ParameterMode.IN)
+                .registerStoredProcedureParameter("p_contrasena", String.class, ParameterMode.IN)
+                .setParameter("p_email", email)
+                .setParameter("p_contrasena", password);
 
+            query.execute();
+
+            Object[] result = (Object[]) query.getSingleResult();
+            Integer idUsuario = Integer.parseInt(result[0].toString());
+            String tipoUsuario = (String) result[1];
+
+            if ("cliente".equals(tipoUsuario)) {
+                return em.find(Cliente.class, idUsuario);
+            } else if ("empleado".equals(tipoUsuario)) {
+                return em.find(Empleado.class, idUsuario);
+            } else {
+                return null;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            em.close();
+        }
+    }
 }
