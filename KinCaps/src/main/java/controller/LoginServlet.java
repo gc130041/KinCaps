06e7ff06926351f27bc13647cc564a55f4c55215
ksolo-2022439 <ksolorzano-2022439@kinal.edu.gt>
2025-07-21 17:25:57 -0600

    feat: Crear LoginServlet para autenticación de usuarios
    Recibe credenciales, consulta al DAO y redirige según el tipo de usuario. Incluye lógica para evitar accesos no autorizados.

---
+++ KinCaps/src/main/java/controller/LoginServlet.java
@@ -0,0 +1,41 @@
+package controller;
+
+import dao.UsuarioDAO;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import modelo.Cliente;
+import modelo.Empleado;
+import modelo.Usuario;
+
+import java.io.IOException;
+
+@WebServlet(name = "LoginServlet", value = "/login")
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        UsuarioDAO usuarioDAO = new UsuarioDAO();
+        Usuario usuario = usuarioDAO.verificarCredenciales(email, password);
+
+        if (usuario != null) {
+            HttpSession session = request.getSession();
+            session.setAttribute("usuario", usuario);
+
+            if (usuario instanceof Cliente) {
+                response.sendRedirect("pages/cliente.jsp");
+            } else if (usuario instanceof Empleado) {
+                response.sendRedirect("pages/empleado.jsp");
+            }
+        } else {
+            request.setAttribute("error", "Correo o contraseña inválidos");
+            request.getRequestDispatcher("index.jsp").forward(request, response);
+        }
+    }
+}
